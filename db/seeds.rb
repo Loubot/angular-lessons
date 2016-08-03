e790e3564320a23acf5c34612616e43fe12df3ba
Louis <lllouis@yahoo.com>
2016-08-03 22:53:10 +0100

    Location working more

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,13 @@ Subject.create(name: 'Guitar', category_id: 1)
 Subject.create(name: 'Drums', category_id: 1)
 Subject.create(name: 'Piano', category_id: 1)
 
-Teacher.create( first_name: "Bob", last_name: "Builder", password: "Football1", email: "lllouis1@yahoo.com")
-Teacher.create( first_name: "Tim", last_name: "Shanks", password: "Football1", email: "lllouis2@yahoo.com")
-Teacher.create( first_name: "Karl", last_name: "Beans", password: "Football1", email: "lllouis3@yahoo.com")
+t1 = Teacher.create( first_name: "Bob", last_name: "Builder", password: "Football1", email: "lllouis1@yahoo.com")
+t2 = Teacher.create( first_name: "Tim", last_name: "Shanks", password: "Football1", email: "lllouis2@yahoo.com")
+t3 = Teacher.create( first_name: "Karl", last_name: "Beans", password: "Football1", email: "lllouis3@yahoo.com")
+
+Location.create( latitude: 53.268382, longitude: -9.056652, name: "Galway", address: "Galway", teacher_id: t1.id )
+Location.create( latitude: 52.665655, longitude: -8.635292, name: "Limerick", address: "Limerick", teacher_id: t2.id )
+Location.create( latitude: 53.344739, longitude: -6.267685, name: "Dublin", address: "Dublin", teacher_id: t3.id )
 
 Teacher.all.each do |t|
   t.subjects << Subject.first
