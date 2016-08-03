1273713147b9faa3de074b244c1640a2c1c2baff
Louis <lllouis@yahoo.com>
2016-08-03 22:56:48 +0100

    Like

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -9,7 +9,7 @@ module SearchHelper
       teachers.as_json( include: [ :photos, :location, :subjects ] )
     else
       p "Searhc helper params #{ params }"
-      subject = Subject.includes(:teachers).where('name ILIKE ?', "%#{ params[:subject_name] }%").select( [ :name, :id ] ).first
+      subject = Subject.includes(:teachers).where('name LIKE ?', "%#{ params[:subject_name] }%").select( [ :name, :id ] ).first
       teachers = subject.teachers.select( "email, id, first_name, last_name" )
       teachers.as_json(include: [ :photos, :location, :subjects ])
     end
