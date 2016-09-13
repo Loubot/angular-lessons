d5c25e9267d76776494546b9cf9e30937e6c50be
Angelini <lllouis@yahoo.com>
2016-09-13 12:26:23 +0100

    WORKED Through some stuff

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -230,6 +230,14 @@
             </html>|
     end
 
+    def convert_hash(hash)
+      hash.each do |k,v|
+        self.instance_variable_set("@#{k}", v.is_a?(Hash) ? Hashit.new(v) : v)
+        self.class.send(:define_method, k, proc{self.instance_variable_get("@#{k}")})
+        self.class.send(:define_method, "#{k}=", proc{|v| self.instance_variable_set("@#{k}", v)})
+      end
+    end
+
     def get_resource_from_auth_hash
       # find or create user by provider and provider uid
       @resource = resource_class.where({
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -66,7 +66,7 @@ class Teacher < ActiveRecord::Base
     # to prevent the identity being locked with accidentally created accounts.
     # Note that this may leave zombie accounts (with no associated identity) which
     # can be cleaned up at a later date.
-    user = signed_in_resource ? signed_in_resource : identity.user
+    user = signed_in_resource ? signed_in_resource : identity.teacher
 
     # Create the user if needed
     if user.nil?
@@ -74,26 +74,26 @@ class Teacher < ActiveRecord::Base
       # Get the existing user by email if the provider gives us a verified email.
       # If no verified email was provided we assign a temporary email and ask the
       # user to verify it on the next step via UsersController.finish_signup
-      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      email = auth.info.email if email_is_verified
-      user = User.where(:email => email).first if email
+      email_is_verified = auth['info']['email'] && (auth['info']['verified'] || auth['info']['verified_email'])
+      email = auth['info']['email'] 
+      user = Teacher.where(:email => email).first if email
 
       # Create the user if it's a new registration
       if user.nil?
-        user = User.new(
-          name: auth.extra.raw_info.name,
+        user = Teacher.new(
+          first_name: auth['info']['name'],
           #username: auth.info.nickname || auth.uid,
-          email: email ? email : "#{TEMP_EMAIL_PREFIX}-#{auth.uid}-#{auth.provider}.com",
+          email: email,
           password: Devise.friendly_token[0,20]
         )
-        user.skip_confirmation!
+        # user.skip_confirmation!
         user.save!
       end
     end
 
     # Associate the identity with the user if needed
-    if identity.user != user
-      identity.user = user
+    if identity.teacher != user
+      identity.teacher = user
       identity.save!
     end
     user
