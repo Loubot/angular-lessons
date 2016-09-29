ba432e906074d74a6e95a98b1c21d336d4e144c8
Louis <lllouis@yahoo.com>
2016-09-29 07:18:01 +0100

    Only search teachers

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -6,13 +6,13 @@ module SearchHelper
       p "county_name and subject_name"
       ids = Location.within( 50, origin: params[:county_name] ).select( [ 'teacher_id' ] ).map( &:teacher_id )
       teachers =  Subject.where( name: params[ :subject_name ] )\
-                  .first.teachers.includes( :photos, :location, :subjects )\
+                  .first.teachers.where( is_teacher: true).includes( :photos, :location, :subjects )\
                   .where( id: ids )
       teachers.as_json( include: [ :photos, :location, :subjects ] )
     else
       p "Search helper params #{ params }"
       subject = Subject.includes(:teachers).where('name ILIKE ?', "%#{ params[:subject_name] }%").select( [ :name, :id ] ).first
-      teachers = subject.teachers.select( "email, id, first_name, last_name" )
+      teachers = subject.teachers.where( is_teacher: true ).select( "email, id, first_name, last_name" )
       teachers.as_json(include: [ :photos, :location, :subjects ])
     end
 
