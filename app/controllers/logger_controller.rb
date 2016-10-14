8653274963be7d55a1f9e54e375b863c42d33f87
Louis <lllouis@yahoo.com>
2016-10-14 18:46:14 +0100

    add rescue

--- app/controllers/logger_controller.rb
+++ app/controllers/logger_controller.rb
@@ -3,15 +3,19 @@ class LoggerController < RailsClientLogger::RailsClientLoggersController
   require 'json'
   def log 
     
-    # logger.fatal params
-    error = JSON.parse params['message']
+    begin 
+      error = JSON.parse params['message']
 
-    if error['data']['errors'][0] != "Authorized users only."
-      logger.info "There hereby follows an error message. Please take note."
-      logger.fatal params
+      if error['data']['errors'][0] != "Authorized users only."
+        logger.info "There hereby follows an error message. Please take note."
+        logger.fatal params
 
+      end
+    rescue NoMethodError
+
+      # render nothing: true
+    ensure
+      render nothing: true
     end
-
-    render nothing: true
   end
 end
