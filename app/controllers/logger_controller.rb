aac674c270d8b0288c5033ec07d2a895fbd623d6
Louis <lllouis@yahoo.com>
2016-10-15 08:55:06 +0100

    get rid of logger

--- app/controllers/logger_controller.rb
+++ app/controllers/logger_controller.rb
@@ -2,20 +2,7 @@ class LoggerController < RailsClientLogger::RailsClientLoggersController
   
   require 'json'
   def log 
-    
-    begin 
-      error = JSON.parse params['message']
-
-      if error['data']['errors'][0] != "Authorized users only."
-        logger.info "There hereby follows an error message. Please take note."
-        logger.fatal params
-
-      end
-    rescue NoMethodError
-
-      # render nothing: true
-    ensure
-      render nothing: true
-    end
+    logger.info params
+   
   end
 end
