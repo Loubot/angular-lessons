efd2b5c9179ef51121ddecb27d925db9f3031cd0
Louis <lllouis@yahoo.com>
2016-10-25 10:54:01 +0100

    Fix logger errors

--- app/controllers/logger_controller.rb
+++ app/controllers/logger_controller.rb
@@ -2,13 +2,14 @@ class LoggerController < RailsClientLogger::RailsClientLoggersController
   
   require 'json'
   def log
-    pp error['data']
-    error = JSON.parse( params[:message] )
+    logger.info params
+    # pp error['data']
+    # error = JSON.parse( params[:message] )
     
-    if error[ 'data' ]['errors'].present?
-      logger.info "There hereby follows an error message. Please take note"
-      logger.info params
-    end
+    # if error[ 'data' ]['errors'].present?
+    #   logger.info "There hereby follows an error message. Please take note"
+    #   logger.info params
+    # end
     render nothing: true
   end
 end
