a725a4e7edc0f3589ffe96c6f1992cf1cbb7e309
Louis <lllouis@yahoo.com>
2016-10-28 19:28:21 +0100

    back we goe

--- config/initializers/compression.rb
+++ config/initializers/compression.rb
@@ -3,7 +3,7 @@
 Rails.application.configure do
   # Use environment names or environment variables:
   # break unless Rails.env.production? 
-  # break unless ENV['ENABLE_COMPRESSION'] == '1'
+  break unless ENV['ENABLE_COMPRESSION'] == '1'
   
   # Strip all comments from JavaScript files, even copyright notices.
   # By doing so, you are legally required to acknowledge
@@ -16,8 +16,6 @@ Rails.application.configure do
 
   config.assets.compile = true
   config.assets.debug = false
-  config.compress_javascript = true
-  config.compress_css = true
 
   config.assets.js_compressor = uglifier
   config.assets.css_compressor = :sass
@@ -25,27 +23,25 @@ Rails.application.configure do
   config.middleware.use Rack::Deflater
   config.middleware.insert_before ActionDispatch::Static, Rack::Deflater
 
-
-
-  # config.middleware.use HtmlCompressor::Rack,
-  #   compress_css: true,
-  #   compress_javascript: true,
-  #   css_compressor: Sass,
-  #   enabled: true,
-  #   javascript_compressor: uglifier,
-  #   preserve_line_breaks: false,
-  #   remove_comments: true,
-  #   remove_form_attributes: false,
-  #   remove_http_protocol: false,
-  #   remove_https_protocol: false,
-  #   remove_input_attributes: true,
-  #   remove_intertag_spaces: false,
-  #   remove_javascript_protocol: true,
-  #   remove_link_attributes: true,
-  #   remove_multi_spaces: true,
-  #   remove_quotes: true,
-  #   remove_script_attributes: true,
-  #   remove_style_attributes: true,
-  #   simple_boolean_attributes: true,
-  #   simple_doctype: false
+  config.middleware.use HtmlCompressor::Rack,
+    compress_css: true,
+    compress_javascript: true,
+    css_compressor: Sass,
+    enabled: true,
+    javascript_compressor: uglifier,
+    preserve_line_breaks: false,
+    remove_comments: true,
+    remove_form_attributes: false,
+    remove_http_protocol: false,
+    remove_https_protocol: false,
+    remove_input_attributes: true,
+    remove_intertag_spaces: false,
+    remove_javascript_protocol: true,
+    remove_link_attributes: true,
+    remove_multi_spaces: true,
+    remove_quotes: true,
+    remove_script_attributes: true,
+    remove_style_attributes: true,
+    simple_boolean_attributes: true,
+    simple_doctype: false
 end
