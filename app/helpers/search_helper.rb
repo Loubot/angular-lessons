187ecb9ea05b3749d54d02929d91f7d4a0448b68
Louis <lllouis@yahoo.com>
2016-11-04 16:43:24 +0000

    more tidying on the search helper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -3,16 +3,24 @@ module SearchHelper
   def search_query( params )
     if params.has_key?( :county_name ) && params.has_key?( :subject_name ) \
       && params[ :county_name ] != "" && params[ :subject_name ] != ""
-      p "county_name and subject_name"
+      p "county_name and subject_name 1111111111111111"
       
       teachers =  geo_return_teachers
       teachers.as_json( include: [ :photos, :location, :subjects ] ).uniq
-    elsif
-      params.has_key?( :county_name ) && !params.has_key?( :subject_name ) \
-      && params[ :county_name ] != ""
+
+    elsif params.has_key?( :county_name ) && params[ :county_name ] != ""
+
+      p "county_name only 2222222222222222"
       ids = Location.within( 50, origin: params[ :county_name ] ).select( [ 'teacher_id' ] ).map( &:teacher_id )
       teachers = Teacher.includes( :photos, :location, :subjects ).find( ids )
-      teachers.as_json( include: [ :photos, :location, :subjects ] )
+      teachers.as_json( include: [ :photos, :location, :subjects ] ).uniq
+
+    elsif params.has_key?( :subject_name ) && params[ :subject_name ] != ""
+
+      p "subject_name only 3333333333333333333"
+      teachers = return_teachers
+      teachers.as_json( include: [ :photos, :location, :subjects ] ).uniq
+
     else
       p "Search helper params #{ params }"
       teachers = return_teachers
