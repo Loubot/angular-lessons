8f8f55d36108b50a9d7a84cbe907977cf0d66545
Louis <lllouis@yahoo.com>
2016-11-08 23:23:31 +0000

    Tests kinda working

--- conf.js
+++ conf.js
@@ -1,19 +1,19 @@
-    exports.config = {
-        allScriptsTimeout: 11000,
-     
-        specs: [
-            'spec/javascripts/controllers/register_controller_spec.js'
-        ],
-     
-        capabilities: {
-            'browserName': 'chrome'
-        },
-     
-        seleniumAddress: 'http://localhost:4444/wd/hub',
-     
-        framework: 'jasmine',
-     
-        jasmineNodeOpts: {
-            defaultTimeoutInterval: 30000
-        }
-    };
+exports.config = {
+    allScriptsTimeout: 11000,
+ 
+    specs: [
+        'spec/javascripts/controllers/register_controller_spec.js'
+    ],
+ 
+    capabilities: {
+        'browserName': 'chrome'
+    },
+ 
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+ 
+    framework: 'jasmine',
+ 
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
--- spec/javascripts/controllers/register_controller_spec.js
+++ spec/javascripts/controllers/register_controller_spec.js
@@ -1,12 +1,20 @@
 describe('Protractor Demo App', function() {
-  it('should add one and two', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
+  it('should hit welcome', function() {
+    browser.get('http://localhost:3000/');
+    expect(browser.getTitle()).toEqual('LYL');
+  });
 
-    element(by.id('gobutton')).click();
+  it('should press the menu button', function(){
+    element(by.css('.md-fab')).click();
+    expect($('md-fab-speed-dial').isDisplayed()).toBe(true);
+  });
 
-    expect(element(by.binding('latest')).getText()).
-        toEqual('3'); // This is wrong!
+  it('should not be logged in', function(){
+    x = element.all(by.css('.md-default-theme')).first();
+    console.log(x);
+    element.all(by.css('.md-default-theme')).then(function(elements) {
+      
+  });
+    
   });
 });
