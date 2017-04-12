8fe8b71867980328d473ac50d80ae9d550aec380
L A <lllouis@yahoo.com>
2017-04-12 14:57:20 +0100

    Try changing default url to www.learnyourlesson.ie

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
-  config.action_mailer.default_url_options = { host: 'http://angular-lessons.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.learnyourlesson.ie' }
 
-  config.action_controller.default_url_options = { host: 'http://angular-lessons.herokuapp.com' }
+  config.action_controller.default_url_options = { host: 'https://www.learnyourlesson.ie' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
