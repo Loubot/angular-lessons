5efbd7f711e96123674a0ca6a78129c0ee45913c
Angelini <lllouis@yahoo.com>
2017-04-21 18:41:57 +0100

    Need to try cards

---
+++ config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "lFF3keOPxIh3f8qBAguamhw3s"
+  config.consumer_secret     = "W5YJGZgHgFfGVu26OfBAc56zIKye4CrHfTeVK2MMY2gPLw4GgL"
+  config.access_token        = "3309922738-Lwci2JlKvRWpQ8MVGHVfcWPMWHqWp7VJcLFfBVW"
+  config.access_token_secret = "BtyYV7kWTkXVT3Hr4r8AQgp1jELco8zXmiIyg8frxmbcg"
+end
