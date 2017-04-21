cabc660e4cfbec649f8d40cb36af8866548af891
Angelini <lllouis@yahoo.com>
2017-04-21 16:37:11 +0100

    try local init

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -4,16 +4,16 @@ class TeacherController < ApplicationController
   require 'pp'
 
   def index
-    # twitter = Twitter::REST::Client.new do |config|
-    #   config.consumer_key        = ENV[ "TWITTER_CONSUMER_KEY" ]
-    #   config.consumer_secret     = ENV[ "TWITTER_CONSUMER_SECRET" ]
-    #   config.access_token        = ENV[ "TWITTER_ACCESS_TOKEN" ]
-    #   config.access_token_secret = ENV[ "TWITTER_ACCESS_SECRET" ]
-    # end
-    pp $twitter
+    twitter = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV[ "TWITTER_CONSUMER_KEY" ]
+      config.consumer_secret     = ENV[ "TWITTER_CONSUMER_SECRET" ]
+      config.access_token        = ENV[ "TWITTER_ACCESS_TOKEN" ]
+      config.access_token_secret = ENV[ "TWITTER_ACCESS_SECRET" ]
+    end
+    pp twitter
     teachers = Teacher.includes( :subjects, :experience, :qualifications, :location, :photos ).where( is_teacher: true ).order( "created_at DESC" )
     #pp teachers
-    $twitter.update("I'm tweeting with @gem!")
+    twitter.update("I'm tweeting with @gem!")
     render json: { teachers: teachers.as_json( include: [ :subjects, :experience, :qualifications, :location, :photos ] ) }
   end
 
