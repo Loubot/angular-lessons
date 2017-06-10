853148f1ff2a8a4f5c50ff2d34dee7cdf9fb735e
Angelini <lllouis@yahoo.com>
2017-06-10 18:45:23 +0100

    debug

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -9,7 +9,7 @@ class AdminController < ApplicationController
       x = $client.update_with_media( tweet_params[ :tweet ][ :text ], Twitter::Image.open_from_url("https://s3-eu-west-1.amazonaws.com/angular-lessons/static_assets/facebook_logo.jpg")  )
     else
       
-      image = Twitter::Image.open_from_url( Photo.find( teacher.profile ).avatar.url ) 
+      image = Teacher.last.photos.last.avatar.file.file
       pp image
       if image[ "status" ] == "failure"
         x = $client.update_with_media( tweet_params[ :tweet ][ :text ], Twitter::Image.open_from_url("https://s3-eu-west-1.amazonaws.com/angular-lessons/static_assets/facebook_logo.jpg")  )
